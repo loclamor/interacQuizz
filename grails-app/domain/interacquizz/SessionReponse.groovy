af926eadd6a4c33639372c7d5923d5bb55a1c175
loclamor <favier.loic.31@gmail.com>
2013-04-08 11:58:17 +0200

    ecriture de la classe SessionReponse

--- grails-app/domain/interacquizz/SessionReponse.groovy
+++ grails-app/domain/interacquizz/SessionReponse.groovy
@@ -1,7 +1,12 @@
 package interacquizz
 
 class SessionReponse {
-
+	Question question
+	static hasMany = [reponses:Reponse]
+	Boolean visible
+	Boolean cloture
+	String cleeAcces
+	
     static constraints = {
     }
 }
